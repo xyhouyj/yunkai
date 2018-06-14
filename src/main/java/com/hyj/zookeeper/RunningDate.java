47d8a546f3096f457e9d9f05b9c0fe25e66b2229
houyunjuan <houyunjuan@DESKTOP-1NTCKG3>
2018-06-14 23:35:53 +0800

    提交人：hyj 提交内容：zooker选举

---
+++ src/main/java/com/hyj/zookeeper/RunningDate.java
@@ -0,0 +1,35 @@
+package com.hyj.zookeeper;
+
+import java.io.Serializable;
+
+public class RunningDate implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private Long cid;
+	
+	private String cname;
+
+	public Long getCid() {
+		return cid;
+	}
+
+	public void setCid(Long cid) {
+		this.cid = cid;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	
+	
+}
